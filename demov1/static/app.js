ef88e4c2bc410a53f6fa2506f1c516f58dab9124
soasme <soasme@gmail.com>
2015-01-06 09:10:15 +0800

    Move comma after expression.

--- demov1/static/app.js
+++ demov1/static/app.js
@@ -1,67 +1,87 @@
 (function($, _, Template){
 
   var app = {
-    model: {}
-  , collection: {}
-  , view: {}
-  , util: {}
-  , router: {}
-  };
+    model: {},
+    collection: {},
+    view: {},
+    util: {},
+    router: {}
+  }
 
-
-  app.util.template = function(id) {
-    return Template.compile($('#' + id).html());
-  };
+  app.util.template = function (id) {
+    return Template.compile(
+      $('#' + id).html()
+    )
+  }
 
   app.model.Node = Backbone.Model.extend({
     urlRoot: '/api/nodes'
-  });
+  })
 
   app.collection.Node = Backbone.Collection.extend({
     model: app.model.Node,
     url: '/api/nodes'
   })
+
   app.view.MainNode = Backbone.View.extend({
     model: app.model.Node
-  });
+  })
 
   app.view.NodeCard = Backbone.View.extend({
-    className: "node-card"
-  , template: app.util.template('node-card-template')
-  , render: function () {
-      this.$el.html(this.template(this.model.attributes));
-      return this;
+    className: "node-card",
+    template: app.util.template('node-card-template'),
+    render: function () {
+      this.$el.html(
+        this.template(
+          this.model.attributes
+        )
+      )
+      return this
     }
-  });
+  })
 
   app.view.NodeSmallCard = Backbone.View.extend({
     className: "node-small-card"
-  });
+  })
 
   app.view.NodePage = Backbone.View.extend({
     className: 'node-page'
-  });
+  })
 
   var MainApp = Backbone.Router.extend({
     routes: {
-      '': "index"
-    , 'i/notification': 'notification'
-    , 'i/discover': 'discover'
-    }
-  , initialize: function (options) {
-    }
-  , index: function() {
-  var node = new app.model.Node({title: 'Title', content: 'Hello World'});
-  node.save();
-  var nodes = new app.collection.Node();
-  console.log(nodes.fetch())
-    }
-  , notification: function () {
-    }
-  , discover: function() {
-    }
-  });
+      '': "index",
+      'i/notification': 'notificationView',
+      'i/discover': 'discoverView',
+      'i/node/:id': 'nodeView'
+    },
 
-  app.router.mainApp = new MainApp();
-  Backbone.history.start({pushState: true});
+    initialize: function (options) {
+    },
+
+    index: function() {
+      // Jump to node 1000001
+      this.navigate("i/node/1000001")
+    },
+
+    notificationView: function () {
+    },
+
+    discoverView: function() {
+    },
+
+    nodeView: function (id) {
+      var node = new app.model.Node({id: id})
+      node.fetch({
+        success: function (model, response, options) {
+          console.log(model)
+        }
+      })
+    }
+  })
+
+  app.router.mainApp = new MainApp()
+
+  Backbone.history.start({pushState: false})
+
 })(jQuery, _, Handlebars);
